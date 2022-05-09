7af534a8b69f5ba495238a40119c0c0731d056f7
mob <mob404@outlook.com>
2022-05-09 17:13:12 +0800

    TT

---
+++ src/Overload/sum0.java
@@ -0,0 +1,23 @@
+package Overload;
+import java.util.Scanner;
+
+public class sum0 {
+    public static void main(String[] args){
+        System.out.println("Plz input n:");
+
+        sum();
+        //System.out.println();
+    }
+
+    public static int sum(){
+        int result=0;
+        Scanner input=new Scanner(System.in);
+        int n= input.nextInt();
+        for(int i=1;i<=n;i++){
+            result=result+i;
+        }
+        System.out.println("1~"+n+"的数字之和为："+result);
+
+        return result;
+    }
+}
